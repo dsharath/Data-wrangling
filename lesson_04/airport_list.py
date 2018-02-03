715890264ff58fcd0e1013c3e44b669419b1dc60
dsharath <dsharathc7@gmail.com>
2018-02-03 17:58:23 -0600

    doc: Adding fourth lesson quiz Q&A

---
+++ lesson_04/airport_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports()' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+
+Refer to the 'options.html' file in the tab above for a stripped down version
+of what is actually on the website. The test() assertions are based on the
+given file.
+"""
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        data = [airport['value'] 
+        for airport in soup.find(id='AirportList')
+            if airport.name=='option' and airport['value'][:3] != 'All']
+        
+        #options=soup.find_all('option')
+        #for k in options:
+         ##      data.append(k['value'])
+
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+if __name__ == "__main__":
+    test()
